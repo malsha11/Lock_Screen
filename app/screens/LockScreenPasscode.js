c9aa585c16ca13aec290fa086c29fcdd76277ae2
malsha11 <96194@fhss.sjp.ac.lk>
2021-10-08 15:24:53 +0530

    important components from react and react-native

---
+++ app/screens/LockScreenPasscode.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+  Dimensions,
+  TouchableHighlight,
+} from 'react-native';
+
+class LockScreenPasscode extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        
+      };
+}
