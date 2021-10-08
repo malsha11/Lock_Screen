7c16b61fd1886508b78757f10055a6498c9d5d20
malsha11 <96194@fhss.sjp.ac.lk>
2021-10-08 16:03:26 +0530

    Manage codeContainer

--- app/screens/LockScreenPasscode.js
+++ app/screens/LockScreenPasscode.js
@@ -14,11 +14,23 @@ class LockScreenPasscode extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       
+        passcode:['1','','3','']
     };
   }
 
-  render() 
+  render() {
+      let numbers = [{id : 1},
+        {id : 1},
+        {id : 2},
+        {id : 3},
+        {id : 4},
+        {id : 5},
+        {id : 6},
+        {id : 7},
+        {id : 8},
+        {id : 9},
+        {id : 0}
+    ];
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content"/>
@@ -26,7 +38,16 @@ class LockScreenPasscode extends Component {
       <View>
           <Text style={styles.passCodeText}> Enter your PIN Code</Text>
       </View>
-     
+      <View style={styles.codeContainer}>
+            {
+                this.state.passcode.map(p => {
+                    let style = p != '' ? styles.code2: styles.code1; 
+                    return <View style={style}></View>;
+                })}
+
+            </View>
+        </View>
+       
            
         </View>
         </SafeAreaView>
@@ -47,4 +68,46 @@ const styles = StyleSheet.create({
     letterSpacing: 0.34,
     lineHeight: 25,
   },
+  codeContainer:{
+      marginTop:12,
+      flexDirection:'row',
+      alignItems:'center',
+      justifyContent:'space-between',
+  },
+  code1: {
+      width:10,
+      height:12,
+      borderRadius:13,
+      borderWidth:2,
+      borderColor:'#1bddf2',
+      margin:8,
+      backgroundColor:'#1bddf2',
+
+  },
+
+  code2: {
+    width:13,
+    height:16,
+    borderRadius:13,
+    borderWidth:2,
+    borderColor:'#0ba39c',
+    margin:8,
+    backgroundColor:'#0ba39c',
+
+},
+  number:{
+      margin:8,
+      width:75,
+      height:75,
+      borderRadius:75,
+      backgroundColor:'rgba(255,255,255,0.1)',
+      justifyContent:'center',
+      alignItems:'center',
+  },
   
+});
+
+
+
+
+
